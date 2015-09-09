79f9470f9daf684a09ed975a5ae397670b87e403
Isto <istobarton@gmail.com>
2015-09-09 10:07:10 -0500

    Added Google Dev Optimization Source

--- GreatestCommonDivisor.js
+++ GreatestCommonDivisor.js
@@ -1,5 +1,7 @@
 //From Code Wars: http://www.codewars.com/kata/5500d54c2ebe0a8e8a0003fd/train/javascript
 
+//Good Article on optimization: https://developers.google.com/speed/articles/optimizing-javascript?hl=en
+
 function mygcd(x,y){
 
   if(x===y){
