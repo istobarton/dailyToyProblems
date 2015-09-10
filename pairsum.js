f23efd0669830903fa0b531ece12d5466960336c
Isto <istobarton@gmail.com>
2015-09-10 10:16:50 -0500

    Completed Simple Solution

--- pairsum.js
+++ pairsum.js
@@ -1,3 +1,11 @@
 //Good article on Array.sort() http://www.javascriptkit.com/javatutors/arraysort.shtml
 
-//
+function largestPairSum(numbers){
+    var sortFunc = function(a, b){
+        return a-b
+    }
+    
+    var sortArr = numbers.sort(sortFunc);
+    
+    return sortArr[sortArr.length-1] + sortArr[sortArr.length-2]
+}
