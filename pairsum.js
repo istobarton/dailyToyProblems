2ba3ab8075a92a179a241ead226805f6830ac923
Isto <istobarton@gmail.com>
2015-09-10 10:19:28 -0500

    Added Top Ranked Solution, Explanation, Code Link

--- pairsum.js
+++ pairsum.js
@@ -9,3 +9,16 @@ function largestPairSum(numbers){
     
     return sortArr[sortArr.length-1] + sortArr[sortArr.length-2]
 }
+
+
+//CODE WARS
+//http://www.codewars.com/kata/556196a6091a7e7f58000018/solutions/javascript
+
+//Top Ranked Solution: 
+
+function largestPairSum(numbers){
+  numbers.sort(function(a, b){ return b - a });
+  return numbers[0] + numbers[1];
+}
+
+//My solution was basically the same, my code could have improved by utilizing the sort function to improve the legibility of my return statement
